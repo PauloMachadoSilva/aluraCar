8180c09ef2291892e929c330dc3746fb83f9d2a7
Macbook <pauloems@yahoo.com.br>
2018-03-27 01:24:25 -0300

    atualizacao do projeto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public carros;
+
   constructor(public navCtrl: NavController) {
+    this.carros = [
+      { nome:'Brasilia' ,preco: '900,00'},
+      { nome:"Gol",preco: "19000,00" },
+      {  nome:"Palio",preco: "11900,00" },
+      {  nome:"Uno",preco: "9900,00" },
+      {  nome:"Siena",preco:'20000,00' },
+      {  nome:"208",preco: '25000,00' },
+      {  nome:"408",preco: '20000,00'},
+      {  nome:"Sandero",preco: '20000,00'},
+      {  nome:"Logan",preco: '20000,00'},
+      {  nome:"Tucson",preco: '20000,00'},
+      {  nome:"Santa Fé",preco: '20000,00'},
+      {  nome:"Sonata",preco: '20000,00'},
+      {  nome:"CLio",preco: '20000,00'},
+      {  nome:"Parati",preco: '20000,00'},
+      {  nome:"Civic",preco: '20000,00'},
+      {  nome:"Corolla",preco: '20000,00'},
+    ]
+
 
   }
 
